fcda4ad45a30efe51936fc0ad2656a3ba05c8a08
Mykola Tokariev <mlangust@mail.ru>
2017-04-23 15:11:57 +0200

    Add pow function

--- Convert Number/src/convert/number/ConvertNumber.java
+++ Convert Number/src/convert/number/ConvertNumber.java
@@ -15,7 +15,17 @@ public class ConvertNumber {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int result = pow(2, 7);
+        System.out.print(result);
+        
+    }
+    public static int pow(int x, int n) {
+        if (n != 1){ // пока n != 1, сводить вычисление pow(x,n) к pow(x,n-1)
+            return x * pow(x, n - 1);
+        } else {
+            return x;
+        }
     }
     
 }
